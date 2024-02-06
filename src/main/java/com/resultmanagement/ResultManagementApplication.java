d4db204061c74a2b458b544cce7d85a49375e2ab
Mobasher Khan <mobasher.khan@confiz.com>
2024-02-06 12:51:36 +0500

    initial project structure

---
+++ src/main/java/com/resultmanagement/ResultManagementApplication.java
@@ -0,0 +1,12 @@
+package com.resultmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResultManagementApplication.class, args);
+    }
+}
